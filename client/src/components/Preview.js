855f628343adf7b07aaf91d6bd73072998079ab7
Chanelle Huang <chanellehuang@gmail.com>
2022-05-31 15:55:35 -1000

    added embedded YouTube videos to Preview page

--- client/src/components/Preview.js
+++ client/src/components/Preview.js
@@ -10,6 +10,43 @@ export default function Preview() {
         Headphones reduce noise and feedback, so you'll get the most accurate
         score.
       </h2>
+      <h3>Watch Bruno Mars sing it!</h3>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/LjhCEhWiKXk"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/GmK5_lnQUbE"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/R7UrFYvl5TE"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/Cwkej79U3ek"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 }
