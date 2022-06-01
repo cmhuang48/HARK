a14fcd6e83f1530673de9324660dfa314beaee3f
Lisa Knox <lisaknox83@gmail.com>
2022-06-01 18:15:15 -0400

    store working, just not well

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { GS, loadCurrentSong } from '../store';
 
-function Home(state, { dispatch }) {
-  console.log(GS, state, dispatch);
-  useEffect(() => loadCurrentSong(2), []);
+function Home(state) {
+  console.log(GS, state);
+  useEffect(() => state.dispatch(loadCurrentSong(2)), []);
 
   return (
     <div className="home">
@@ -16,8 +16,8 @@ function Home(state, { dispatch }) {
 const mapDispatch = (dispatch) => {
   return {
     // genericGet: (route, model) => dispatch(GS.genericGet(route, model)),
-    loadCurrentSong: (songId) => dispatch(loadCurrentSong(songId)),
+    // loadCurrentSong: (songId) => dispatch(loadCurrentSong(songId)),
   };
 };
 
-export default connect((state) => state, mapDispatch)(Home);
+export default connect((state) => state, null)(Home);
