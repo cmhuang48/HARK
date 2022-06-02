f4e036faf903e7c029b13cbbb93878688d44254d
Lisa Knox <lisaknox83@gmail.com>
2022-06-02 13:25:44 -0400

    store up

--- server/index.js
+++ server/index.js
@@ -1,42 +1,42 @@
 const {
   db,
   models: { Song, Artist },
-} = require("../server/db");
+} = require('../server/db');
 const port = process.env.PORT || 8080;
-const app = require("./app");
+const app = require('./app');
 
 const init = async () => {
   try {
-    console.log("calling init");
+    console.log('calling init');
     await db.sync({ force: true });
 
     const [brunoMars, kellyClarkson, neilDiamond, vanessaCarlton] =
       await Promise.all([
-        Artist.create({ name: "Bruno Mars" }),
-        Artist.create({ name: "Kelly Clarkson" }),
-        Artist.create({ name: "Neil Diamond" }),
-        Artist.create({ name: "Vanessa Carlton" }),
+        Artist.create({ name: 'Bruno Mars' }),
+        Artist.create({ name: 'Kelly Clarkson' }),
+        Artist.create({ name: 'Neil Diamond' }),
+        Artist.create({ name: 'Vanessa Carlton' }),
       ]);
 
     const songs = await Promise.all([
       Song.create({
-        name: "Just The Way You Are",
-        audioURL: "test",
+        name: 'Just The Way You Are',
+        audioURL: 'test',
         artistId: brunoMars.id,
       }),
       Song.create({
-        name: "Since U Been Gone",
-        audioURL: "test",
+        name: 'Since U Been Gone',
+        audioURL: 'test',
         artistId: kellyClarkson.id,
       }),
       Song.create({
-        name: "Sweet Caroline",
-        audioURL: "test",
+        name: 'Sweet Caroline',
+        audioURL: 'test',
         artistId: neilDiamond.id,
       }),
       Song.create({
-        name: "A Thousand Miles",
-        audioURL: "test",
+        name: 'A Thousand Miles',
+        audioURL: 'test',
         artistId: vanessaCarlton.id,
       }),
     ]);
@@ -48,3 +48,5 @@ const init = async () => {
 };
 
 init();
+
+module.exports = { port };
