089950ca13085071e78bb2328469950ebc9437bd
Lisa Knox <lisaknox83@gmail.com>
2022-06-22 14:29:23 -0400

    changed sampling rate

--- client/src/components/Pitch.js
+++ client/src/components/Pitch.js
@@ -1,12 +1,12 @@
-import React, { useCallback } from "react";
-import { connect } from "react-redux";
-import { useParams } from "react-router-dom";
-import { useModel } from "react-tensorflow";
-import * as tf from "@tensorflow/tfjs";
-import { Box, Button } from "@mui/material";
+import React, { useCallback } from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useModel } from 'react-tensorflow';
+import * as tf from '@tensorflow/tfjs';
+import { Box, Button } from '@mui/material';
 
 const NUM_INPUT_SAMPLES = 1024;
-const MODEL_SAMPLE_RATE = 16000;
+const MODEL_SAMPLE_RATE = 3000;
 const PT_OFFSET = 25.58;
 const PT_SLOPE = 63.07;
 const CONF_THRESHOLD = 0.9;
@@ -27,12 +27,12 @@ function Pitch({ pitches, setPitches, songs, pitchData }) {
 
   const handleSuccess = useCallback((stream, model) => {
     let context = new AudioContext({
-      latencyHint: "playback",
+      latencyHint: 'playback',
       sampleRate: MODEL_SAMPLE_RATE,
     });
 
-    const retryButton = document.getElementById("retryButton");
-    retryButton.addEventListener("click", async () => {
+    const retryButton = document.getElementById('retryButton');
+    retryButton.addEventListener('click', async () => {
       context.close();
       setPitches([]);
       window.location.reload();
@@ -46,7 +46,7 @@ function Pitch({ pitches, setPitches, songs, pitchData }) {
     );
 
     // Converts audio to mono.
-    processor.channelInterpretation = "speakers";
+    processor.channelInterpretation = 'speakers';
     processor.channelCount = 1;
 
     // Runs processor on audio source.
@@ -99,8 +99,8 @@ function Pitch({ pitches, setPitches, songs, pitchData }) {
     const averageErrorRate =
       errorRates.reduce((accum, rate) => accum + rate, 0) / errorRates.length;
     const score = (1 - averageErrorRate) * 100;
-    console.log("score is", score);
-    window.localStorage.setItem("score", score);
+    console.log('score is', score);
+    window.localStorage.setItem('score', score);
     window.location.href = `/score/${id}`;
   };
 
@@ -108,21 +108,21 @@ function Pitch({ pitches, setPitches, songs, pitchData }) {
     <div className="pitch">
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
         <Button
           id="retryButton"
           variant="contained"
           sx={{
-            marginTop: "15px",
-            bgcolor: "#1F2833",
-            "&:hover": { bgcolor: "#45A29E" },
+            marginTop: '15px',
+            bgcolor: '#1F2833',
+            '&:hover': { bgcolor: '#45A29E' },
           }}
-          style={{ m: 1, width: "50%", padding: "10px", fontSize: "1rem" }}
+          style={{ m: 1, width: '50%', padding: '10px', fontSize: '1rem' }}
         >
           Restart
         </Button>
@@ -131,11 +131,11 @@ function Pitch({ pitches, setPitches, songs, pitchData }) {
           onClick={handleClick}
           variant="contained"
           sx={{
-            marginTop: "15px",
-            bgcolor: "#1F2833",
-            "&:hover": { bgcolor: "#45A29E" },
+            marginTop: '15px',
+            bgcolor: '#1F2833',
+            '&:hover': { bgcolor: '#45A29E' },
           }}
-          style={{ m: 1, width: "50%", padding: "10px", fontSize: "1rem" }}
+          style={{ m: 1, width: '50%', padding: '10px', fontSize: '1rem' }}
         >
           Calculate Score
         </Button>
--- client/src/components/SingAlong.js
+++ client/src/components/SingAlong.js
@@ -1,10 +1,10 @@
-import React, { useState, useCallback } from "react";
-import { connect } from "react-redux";
-import { useParams } from "react-router-dom";
-import ReactAudioPlayer from "react-audio-player";
-import { Box, Typography } from "@mui/material";
-import Lyric from "./Lyric";
-import Pitch from "./Pitch";
+import React, { useState, useCallback } from 'react';
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import ReactAudioPlayer from 'react-audio-player';
+import { Box, Typography } from '@mui/material';
+import Lyric from './Lyric';
+import Pitch from './Pitch';
 
 function SingAlong({ songs, artists, pitchData }) {
   const { id } = useParams();
@@ -32,35 +32,35 @@ function SingAlong({ songs, artists, pitchData }) {
     const averageErrorRate =
       errorRates.reduce((accum, rate) => accum + rate, 0) / errorRates.length;
     const score = (1 - averageErrorRate) * 100;
-    console.log("score is", score);
-    window.localStorage.setItem("score", score);
+    console.log('score is', score);
+    window.localStorage.setItem('score', score);
   };
 
   return (
     <div className="singAlong">
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
         <video src="/../images/spotlight.mp4" muted loop autoPlay></video>
         <div className="content">
           <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
             }}
           >
             <Typography
               variant="h3"
               fontFamily="Arvo"
               color="#66FCF1"
-              sx={{ marginBottom: "10px" }}
+              sx={{ marginBottom: '10px' }}
             >
               {song?.name} by {artist?.name}
             </Typography>
